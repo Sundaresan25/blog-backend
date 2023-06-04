905caf0aa2872ecdbe902d0650fd03d4bc577b2d
sundar <sundarbgm98@gmail.com>
2023-06-04 10:07:47 +0530

    minor bugs

--- src/middleware/authenticate.js
+++ src/middleware/authenticate.js
@@ -2,21 +2,24 @@ const jwt = require("jsonwebtoken");
 
 // Middleware function to authenticate the token
 const authenticate = (req, res, next) => {
-  try {
-    const token = req.headers.authorization.split(" ")[1];
-    const decode = jwt.verify(token, "secretValue");
-    req.user = decode;
-    next();
-  } catch (error) {
-    if (error.name === "TokenExpiredError") {
-      res.status(401).json({
-        message: "Token Expired",
-      });
-    }
-    res.json({
-      message: "Authentication failed!",
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) {
+    return res.status(401).json({
+      message: "Unauthorized",
     });
   }
+
+  jwt.verify(token, "secretValue", (err, user) => {
+    if (err) {
+      return res.status(403).json({
+        message: "Forbidden",
+      });
+    }
+    req.user = user;
+    next();
+  });
 };
 
 module.exports = authenticate;
